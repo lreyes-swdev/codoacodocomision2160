daa841c5b53bda9ac2e4ec16aa51c6455adc79b4
lreyes-swdev <lreyes.swdev@gmail.com>
2021-10-01 14:25:08 -0300

    EventListener, Funciones anónimas, y más

--- javascript/js/clase3.js
+++ javascript/js/clase3.js
@@ -3,10 +3,10 @@ function calcular() {
     let num = Number(prompt("Ingrese el primer número"));
     let num2 = Number(prompt("Ingrese el segundo número"));
     let op = prompt("Elija el tipo de operación: \n" +
-                    " + Para sumar \n" +
-                    " - Para restar\n" +
-                    " * Para multiplicar\n" +
-                    " / Para dividir");
+        " + Para sumar \n" +
+        " - Para restar\n" +
+        " * Para multiplicar\n" +
+        " / Para dividir");
     let resultado = 0;
     let elemento = document.getElementById("principal");
 
@@ -26,5 +26,7 @@ function calcular() {
         resultado = num * num2;
         elemento.innerHTML += "<p> El resultado de la multiplicación de " + num + " y " + num2 + " es: " +
             resultado + "</p>";
+    } else {
+        elemento.innerHTML += "<p> El valor para la operación es incorrecto!</p>";
     }
 }
